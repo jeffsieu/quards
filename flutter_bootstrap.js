a8cc673e71193125ab83b2e6ff1881a7a50ef1e1
jeffsieu <jeffsieu@users.noreply.github.com>
2024-07-03 17:37:39 +0000

    deploy: 4099139d90c201a28f092e5f384aed27ca9dd42b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "942547740"
+    serviceWorkerVersion: "367012034"
   }
 });
